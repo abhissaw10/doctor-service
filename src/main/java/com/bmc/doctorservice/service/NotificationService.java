222c4ecb4756d2d8ff7744a45409a7dbdacfc3c1
abhissaw10 <sawhney.abhishek1983@gmail.com>
2021-07-13 07:51:10 +0530

    Doctor email id verification on SeS

--- src/main/java/com/bmc/doctorservice/service/NotificationService.java
+++ src/main/java/com/bmc/doctorservice/service/NotificationService.java
@@ -6,8 +6,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import static com.bmc.doctorservice.model.DoctorStatus.PENDING;
+
 @Service
 public class NotificationService {
+
+    @Autowired
+    SeSEmailVerification emailVerification;
 
     @Autowired
     KafkaTemplate<String, Doctor> kafkaTemplate;
@@ -16,6 +21,9 @@ public class NotificationService {
     private String doctorRegistrationNotificationTopic;
 
     public void notifyDoctorRegistration(Doctor doctor){
+        if(PENDING.value.equals(doctor.getStatus())) {
+            emailVerification.sendVerificationEmail(doctor.getEmailId());
+        }
         kafkaTemplate.send(doctorRegistrationNotificationTopic,doctor);
     }
 }
